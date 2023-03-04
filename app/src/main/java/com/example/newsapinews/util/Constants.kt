054e6cfb43f9efca9f3edfea687ac39c9c3e2e88
Abhishek Nayak <abhishek.nayak560@gmail.com>
2023-03-04 21:54:14 +0530

    Revert "utils package added"
    This reverts commit 1d9e5361e1ae3a9a9989b21e0aaf9b9a5120f634.

--- app/src/main/java/com/example/newsapinews/util/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.newsapinews.util
-
-object Constants {
-    const val base_url=""
-    const val api_key=""
-    const val breakingNewsEndPoint=""
-    const val allNewsEndPoint=""
-}
--- app/src/main/java/com/example/newsapinews/util/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsapinews.util
-
-sealed class Resource<T>(val data:T?=null,val msg:String?=null){
-    class Success<T>(data: T):Resource<T>(data)
-    class Failure<T>(msg: String,data: T?):Resource<T>(data, msg)
-    class Loading<T>:Resource<T>()
-}
