1d9e5361e1ae3a9a9989b21e0aaf9b9a5120f634
Abhishek Nayak <abhishek.nayak560@gmail.com>
2023-03-04 21:52:34 +0530

    utils package added

---
+++ app/src/main/java/com/example/newsapinews/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapinews.util
+
+object Constants {
+    const val base_url=""
+    const val api_key=""
+    const val breakingNewsEndPoint=""
+    const val allNewsEndPoint=""
+}
